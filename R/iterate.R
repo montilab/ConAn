2ff72d586216c75f4a86929a9edfab7edb269060
Oluwatosin Olayinka <oaolayin@ad.unc.edu>
2021-01-28 12:10:10 -0500

    modified background function to deal with alternate sample sizes

--- R/iterate.R
+++ R/iterate.R
@@ -88,15 +88,25 @@ iter_differential_connectivity <- function(iter,
 
 
 #' @keywords internal
-do_background <- function(iter, c_edat, mods, mean_correct) {
+do_background <- function(iter, c_edat, mods, mean_correct, sample_sizes=c()) {
+	alt_sample_size <- is.null(sample_sizes)
+	if(sample_sizes[1] > length(iter$samples_r)) {
+		stop(paste("Sample size of group r," sample_sizes[1], "can not be greater than the length of iter$sample_r"))
+	}
 
-    if (mean_correct) {
-        
-        iter[['bg_r']] <- c_edat[iter$samples_r,] %>%
+	if(sample_sizes[2] > length(iter$samples_t)) {
+		stop(paste("Sample size of group t," sample_sizes[2], "can not be greater than the length of iter$sample_t"))
+	}
+	
+    if (mean_correct) {		
+		r_n <- ifelse(alt_sample_size, sample_sizes[1], length(iter$samples_r))
+        t_n <- ifelse(alt_sample_size, sample_sizes[2], length(iter$samples_t))
+
+		iter[['bg_r']] <- c_edat[iter$samples_r[1:r_n],] %>%
                           atanh_lower_tri_erase_mods_cor(mods=mods) %>%
                           mean(na.rm=TRUE)
 
-        iter[['bg_t']] <- c_edat[iter$samples_t,] %>%
+        iter[['bg_t']] <- c_edat[iter$samples_t[1:t_n],] %>%
                           atanh_lower_tri_erase_mods_cor(mods=mods) %>%
                           mean(na.rm=TRUE)
 
