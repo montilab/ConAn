2c733ec629a7731dadc77d6f365eed1cd3fc5de7
Lina Kroehling <72235521+lkroeh@users.noreply.github.com>
2021-03-07 16:39:22 -0500

    Update plotting.R

--- R/plotting.R
+++ R/plotting.R
@@ -8,11 +8,16 @@ plot_connectivity <- function(output,N_genes) {
     r_name <- output$args$ctrl
     t_name <- output$args$cond
     
+    low_r_IQR <- quantile(cv_r, 0.25) - (1.5 * (quantile(cv_r, 0.75) - quantile(cv_r, 0.25)))
+    high_r_IQR <- quantile(cv_r, 0.75) + (1.5 * (quantile(cv_r, 0.75) - quantile(cv_r, 0.25)))
+    low_t_IQR <- quantile(cv_t, 0.25) - (1.5 * (quantile(cv_t, 0.75) - quantile(cv_t, 0.25)))
+    high_t_IQR <- quantile(cv_t, 0.75) + (1.5 * (quantile(cv_t, 0.75) - quantile(cv_t, 0.25)))
+    
     df <- data.frame(Median = c(median(cv_r), median(cv_t)),
-                      Min = c(min(cv_r), min(cv_t)),
-                      Lower = c(quantile(cv_r, 0.25), min(quantile(cv_t, 0.25))),
-                      Upper = c(quantile(cv_r, 0.75), min(quantile(cv_t, 0.75))),
-                      Max = c(max(cv_r), max(cv_t)),
+                      Min = c(min(cv_r[cv_r >= low_r_IQR]), min(cv_t[cv_t >= low_t_IQR])),
+                      Lower = c(quantile(cv_r, 0.25), quantile(cv_t, 0.25)),
+                      Upper = c(quantile(cv_r, 0.75), quantile(cv_t, 0.75)),
+                      Max = c(max(cv_r[cv_r <= high_r_IQR]), max(cv_t[cv_t <= high_t_IQR])),
                       Group = c(r_name, t_name))
 
     
@@ -24,11 +29,16 @@ plot_connectivity <- function(output,N_genes) {
       r_bg_name <- paste(r_name, "(BG)")
       t_bg_name <- paste(t_name, "(BG)")
       
+      low_rbg_IQR <- quantile(cv_r_bg, 0.25) - (1.5 * (quantile(cv_r_bg, 0.75) - quantile(cv_r_bg, 0.25)))
+      high_rbg_IQR <- quantile(cv_r_bg, 0.75) + (1.5 * (quantile(cv_r_bg, 0.75) - quantile(cv_r_bg, 0.25)))
+      low_tbg_IQR <- quantile(cv_t_bg, 0.25) - (1.5 * (quantile(cv_t_bg, 0.75) - quantile(cv_t_bg, 0.25)))
+      high_tbg_IQR <- quantile(cv_t_bg, 0.75) + (1.5 * (quantile(cv_t_bg, 0.75) - quantile(cv_t_bg, 0.25)))
+    
       df <- rbind(df, data.frame(Median = c(median(cv_r_bg), median(cv_t_bg)),
-                        Min = c(min(cv_r_bg), min(cv_t_bg)),
-                        Lower = c(quantile(cv_r_bg, 0.25), min(quantile(cv_t_bg, 0.25))),
-                        Upper = c(quantile(cv_r_bg, 0.75), min(quantile(cv_t_bg, 0.75))),
-                        Max = c(max(cv_r_bg), max(cv_t_bg)),
+                        Min = c(min(cv_r_bg[cv_r_bg >= low_rbg_IQR]), min(cv_t_bg[cv_t_bg >= low_tbg_IQR])),
+                        Lower = c(quantile(cv_r_bg, 0.25), quantile(cv_t_bg, 0.25)),
+                        Upper = c(quantile(cv_r_bg, 0.75), quantile(cv_t_bg, 0.75)),
+                        Max = c(max(cv_r_bg[cv_r_bg <= high_rbg_IQR]), max(cv_t_bg[cv_t_bg <= high_tbg_IQR])),
                         Group = c(r_bg_name, t_bg_name)))
     }
     
