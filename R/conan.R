1ae706fa9226bc9511346bfd8fc225d4371681eb
Lina Kroehling <72235521+lkroeh@users.noreply.github.com>
2021-03-07 20:51:06 -0500

    Update conan.R

--- R/conan.R
+++ R/conan.R
@@ -317,7 +317,7 @@ conan <- function(eset,
     }
     if (reporting) {
         cat("Generating report...\n")
-        report(output)
+        report(output, mod_list)
     }
 
     cat("Successful finish...\n")
