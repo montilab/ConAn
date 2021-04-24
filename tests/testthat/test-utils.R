86f22897838986c1e27d726ce46a916613f7c9b9
Lina Kroehling <lkroeh@bu.edu>
2021-04-24 12:44:38 -0400

    testing for functions in utils.R

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,33 @@
+test_that("lower_tri works", {
+  mat <- matrix(1:9, nrow = 3, ncol = 3)
+  expect_is(mat, "matrix")
+  mat_lowertri_diag <- lower_tri(mat, diag = TRUE)
+  expect_is(mat_lowertri_diag, "integer")
+  mat_lowertri <- lower_tri(mat, diag = FALSE)
+  expect_equal(sum(mat_lowertri_diag), 26)
+  expect_is(mat_lowertri, "integer")
+  expect_equal(sum(mat_lowertri), 11)
+})
+
+test_that("remove_na works", {
+  vec <- c("test", "test1", NA, "test2")
+  expect_is(vec, "character")
+  vecNA <- vec[!is.na(vec)]
+  rmNA <- remove_na(vec)
+  expect_equal(length(vec), 4)
+  expect_equal(length(vecNA), 3)
+  expect_equal(rmNA, vecNA)
+})
+
+test_that("subtract_BG works", {
+  mat <- matrix(1:9, nrow = 3, ncol = 3)
+  bgmat <- mat - 1
+  expect_is(mat, "matrix")
+  expect_is(bgmat, "matrix")
+  subtract <- mat-bgmat
+  subtractfn <- subtract_bg(mat, bgmat)
+  expect_equal(sum(subtract), 9)
+  expect_equal(subtract, subtractfn)
+})
+
+
